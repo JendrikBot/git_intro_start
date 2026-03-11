6d02c7456017c25277ce27b328ab4cf1f43c4055
Wouter <devriese.wouter@gmail.com>
2026-03-11 22:14:23 +0100

    updated project.ino om interessantere issues te verkijgen

--- arduino/project/project.ino
+++ arduino/project/project.ino
@@ -1,10 +1,15 @@
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);                      
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(1000);                      
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
+  Serial.println("Hello World");
+  delay(2000);
 }
